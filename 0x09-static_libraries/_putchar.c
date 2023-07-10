fa884b13bedf4cdd6f215be8c2b52e43064ac36e
Aliceit210 <uwimana21445@gmail.com>
2023-07-10 16:51:56 -0700

    alx

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdio.h>
+/**
+ * _putchar - write the character c to stdout
+ * @c: Print character
+ * Return: on success 1.
+ * on error, -1 is returned.
+ */
+
+int _putchar(char c)
+{
+		return putchar(c);
+}
