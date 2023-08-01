c35940faea750d062238af09e49a7aa77ce7da00
root <Aliceit210>
2023-08-01 06:15:08 -0700

    0x09. C - Static libraries

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,23 +1,26 @@
-#include "main.h"
-
 /**
- * _isupper - checks for uppercase character
- * @letter: the chracter to be tracked
- * Return: 1 if c is uppercase, 0 otherwise
+ * _strcat - function that concatenates
+ * two strings.
+ *
+ * @dest: pointer to destnation input
+ * @src: pointer to source input
+ *
+ * Return: pointer to resulting string @dest
  */
 
-
-
-int _isupper(int letter)
-
-
-
+char *_strcat(char *dest, char *src)
 {
+	int c, c2;
 
+	c = 0;
+	/*find the size of dest array*/
+	while (dest[c])
+		c++;
 
+	/* iterate through each src array value without the null byte*/
+	for (c2 = 0; src[c2] ; c2++)
+		/*append src[c2] to dest[c] while overwritting the null byte in dest*/
+		dest[c++] = src[c2];
 
-	return (letter >= 'A' && letter <= 'Z');
-
-
-
+	return (dest);
 }
