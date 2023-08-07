4e0b46a9b111aeff004ca7f3fd298d12194709cf
root <Aliceit210>
2023-08-07 15:11:36 -0700

    0x15. C - File I/O

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -16,4 +16,4 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif /* MAIN_H *e
+#endif /* MAIN_H */
